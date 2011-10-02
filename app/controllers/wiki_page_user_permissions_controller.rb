fd95c2a9c533eda40c534d6f7d0fd3ffd1303ab6
Igor Zubkov <igor.zubkov@gmail.com>
2011-10-02 23:09:52 +0300

    Cleanup

--- app/controllers/wiki_page_user_permissions_controller.rb
+++ app/controllers/wiki_page_user_permissions_controller.rb
@@ -3,9 +3,9 @@ class WikiPageUserPermissionsController < ApplicationController
 
   def destroy
     WikiPageUserPermission.find(params[:id]).destroy
-  	redirect_to :back
+    redirect_to :back
   end
-  
+
   def update
     params[:wiki_page_user_permission].each_pair do |index, level|
       permission = WikiPageUserPermission.find index.to_i
